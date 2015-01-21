173326a86a54d6719d194a3e7ac80bd20df5fd20
Zhengshi Lucia <zhengshizhao917@gmail.com>
2015-01-20 22:52:28 -0500

    add result as comment

--- check.R
+++ check.R
@@ -6,4 +6,13 @@ mdat <- matrix(c(1,2, 11,12), nrow = 2, ncol = 2, byrow = TRUE,
 matrx <- makeCacheMatrix(mdat)
 
 cacheSolve(matrx)
+##    row1 row2
+##C.1 -1.2  0.2
+##C.2  1.1 -0.1
 
+cacheSolve(matrx)
+
+##getting cached matrix
+##row1 row2
+##C.1 -1.2  0.2
+##C.2  1.1 -0.1
